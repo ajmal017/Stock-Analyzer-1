897060c6904c723a63ff80f3e41c2946729767d6
jmucha90 <46336522+jmucha90@users.noreply.github.com>
2020-05-10 19:30:30 -0400

    Default candlestick chart theme is dark theme

--- candlestick.py
+++ candlestick.py
@@ -15,7 +15,7 @@ class candlestick:
 
     # Gets historical data for the selected stock
     def history(self, ticker):
-        return yf.Ticker(str(ticker)).history(period='max')
+        return yf.Ticker(str(ticker)).history(period='5y')
 
     # Creates candlestick chart in the form of a Plotly graph
     def graph(self, ticker):
@@ -24,9 +24,22 @@ class candlestick:
                                              open=stockdata['Open'],
                                              high=stockdata['High'],
                                              low=stockdata['Low'],
-                                             close=stockdata['Close'])])
+                                             close=stockdata['Close'],
+                                             name = 'Candlesticks')])
+
+
+        fig.add_trace(go.Scatter(x=stockdata.index, y=stockdata['Close'].rolling(10).mean(), name = 'Rolling Average-10 Days',
+                                 line=dict(color='white', width=2)))
+
+        fig.add_trace(go.Scatter(x=stockdata.index, y=stockdata['Close'].rolling(30).mean(), name = 'Rolling Average-30 Days',
+                                 line=dict(color='#003300', width=2)))
+
+        fig.add_trace(go.Scatter(x=stockdata.index, y=stockdata['Close'].rolling(60).mean(), name = 'Rolling Average-60 Days',
+                                 line=dict(color='blue', width=2)))
+
         fig.update_layout(title=str(ticker) + '\'s Candlestick Chart',
                           xaxis_title='Date',
-                          yaxis_title='USD')
+                          yaxis_title='USD',
+                          template='plotly_dark')
         graph = fig.show()
         return graph
